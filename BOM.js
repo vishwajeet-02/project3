32e7823fd44c77aa0e4a9c1aa3a82dfe9a45e90c
vishwajeet <vwjchindariya473443@gmail.com>
2026-05-26 01:41:53 +0530

    add some deatail

---
+++ BOM.js
@@ -0,0 +1,152 @@
+// programe for objects
+
+// const mobile = {
+//     mobileName: "one plus",
+//     ram: 64,
+//     camera: 24,
+//     price: 20000
+    
+// };
+// console.log(mobile.ram);
+
+
+//programe of  use operator  for student result
+// let score = prompt("enter your score (0-100):");
+// let grade;
+// if(score >=90 && score<=100){
+//     grade= "A"
+// }
+// else if (score >=80 && score <=90){
+//     grade = "B"
+// }
+// else if (score >=70 && score <=80){
+//     grade = "C"
+// }
+// else if (score >=60 && score <=70){
+//     grade = "D"
+// }
+// else if (score >=40 && score <=50){
+//     grade = "F"
+// }
+
+
+// console.log("grade=", grade);
+//programe for studend record
+// let record ={
+//     name:"drishti",
+//     age:3,
+//     player: "cricket"
+
+// }
+// console.log(record.name);
+// programe for vote elegiblity
+// for(let age = 0; age <= 100; age++){
+//     if(age >=18){
+//         console.log("you are elegible for vote");
+//     }
+//     else{
+//         console.log("you are not elegible for vote");
+//     }
+// }
+//for loop 
+// let sum = 0;
+// for (let i=1; i<=5; i++){
+//     sum=sum +i;
+// }
+// console.log("sum=" ,sum);
+//while loop condition pehlr hi check hoti hai
+// let i= 1;
+// while(i <=5){
+//     console.log("i=",i);
+//     i++;
+    
+// }
+//do while loop condition last me check hoti hai
+//  let i= 1;
+//  do{
+//     console.log("i=", i);
+//      i++;
+    
+// }
+// while(i <= 5);
+//for -of loop it used for string variable and arrays
+// let str ="ankur";
+// let size =0
+// for(let i of str){
+//     console.log("i=",i);
+//    size++; 
+// }
+// console.log("size=", size);
+//for =in loop used for objects its loop return ket valuse of object
+// let student ={
+//     name: "ankur",
+//     age: 24,
+//     cgpa: 8.00,
+//     pass: "true"
+// }
+// for (let key in student){
+// console.log("key=",key, "value=",student[key]);
+// }
+// for print 0 to 1oo
+// for (let num=0; num<=100; num++){
+//     console.log("num=",num);
+    
+// }
+// print all even num from 0 to 100
+// for(let i=0; i<=100; i++){
+//     if(i%2===0){
+//         console.log("i=", i);
+        
+//     }
+//     else{
+
+    
+// }
+// }
+
+//for odd no, print
+// for(let i=0; i<=100; i++){
+//     if(i%2!==0){
+//         console.log("i=", i);
+        
+//     }
+//     else{
+
+    
+// }
+// }
+//for game
+// let gameNum=30;
+// let userNum = prompt(" gusse the game number: ");
+// while(userNum != gameNum){
+//     userNum=("you enter worng Number. gusse again:")
+
+// }
+// console.log("conratulation, you entered right number");
+// let cha= "ankur";
+// console.log(cha.length);
+
+// let cha = "ankur"
+// console.log(cha.toUpperCase());
+// let cha = "ankur";
+// console.log(cha.toLowerCase);
+// let cha1="ankur"
+// let cha2="chindariya"
+// console.log(cha1+cha2);
+// let cha = "ankur"
+// console.log(cha.slice("2"));
+// let cha="ankur";
+// console.log(cha.replace("n","l"));
+// let cha ="ankur";
+// console.log(cha.replaceAll("ankur","chindariya"));
+
+
+
+
+
+
+
+
+
+
+
